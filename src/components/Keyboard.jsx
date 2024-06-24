f9fdf7a11a9222e6c4f5f090275ba1737c80e7fa
BossNPtl <boss.dowork@gmail.com>
2024-06-24 16:46:36 +0700

    fix-name

--- src/components/Keyboard.jsx
+++
@@ -1,9 +0,0 @@
-import keyboardIOS from "../assets/keyboardIOS.png";
-
-const Keyboard = () => (
-  <div className="min-w-[378px] min-h-[430px] " style={{ marginTop: "65px" }}>
-    <img src={keyboardIOS} alt="Keyboard IOS" />
-  </div>
-);
-
-export default Keyboard;
--- src/hooks/customerHook.js
+++
@@ -1,6 +0,0 @@
-import { useContext } from "react";
-import { CustomerContext } from "../contexts/customerContext";
-
-export default function UseCustomer() {
-    return useContext(CustomerContext);
-}
