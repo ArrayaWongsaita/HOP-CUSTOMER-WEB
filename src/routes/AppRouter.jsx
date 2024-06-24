4b89bb16a602353fb44529593acadae4acbba16c
pratchata <ratchata.pyc@gmail.com>
2024-06-24 15:11:48 +0700

    config-homepage and change name register

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,27 +1,31 @@
 import { RouterProvider } from "react-router-dom";
 import { createBrowserRouter } from "react-router-dom";
-
-import RegisterAndLoginPage from "../pages/registerAndLogin-page";
+import RegisterAndLoginPage from "../Pages/RegisterAndLogin-page";
 import MainContainer from "../layouts/mainContainer";
-import HomePage from "../pages/home-page";
+import Homepage from "../Pages/Homepage";
+
 // import ProtectedRoute from "../features/protectedRoute";
 
 const router = createBrowserRouter([
-    { path: '/auth/register', element: <div className="w-lvw h-lvh bg-black flex justify-center"><RegisterAndLoginPage /></div> },
-    {
-        path: '/', element: (
-            // <ProtectedRoute>
-                <MainContainer />
-            // </ProtectedRoute>
-        ),
-        children: [
-            { path: '/', element: <HomePage /> }
-        ]
-    }
-])
+  {
+    path: "/auth/register",
+    element: (
+      <div className="w-lvw h-lvh bg-black flex justify-center">
+        <RegisterAndLoginPage />
+      </div>
+    ),
+  },
+  {
+    path: "/",
+    element: (
+      // <ProtectedRoute>
+      <MainContainer />
+      // </ProtectedRoute>
+    ),
+    children: [{ path: "/", element: <Homepage /> }],
+  },
+]);
 
 export default function AppRouter() {
-    return (
-        <RouterProvider router={router} />
-    )
+  return <RouterProvider router={router} />;
 }
